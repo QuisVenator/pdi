3ae0b0cd1e15be870f6482c52c126158a6e17538
Manuel Rene Pauls Toews <rene.pauls@avantys.de>
2024-04-07 13:50:52 -0400

    make changes :P

--- algorithm.py
+++ algorithm.py
@@ -4,12 +4,21 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
+DEBUG = True
 
 
 def get_separate_point(hist, total_pixels):
     gray_level_maximum_range = hist.shape[0]
     total_sum = sum([ hist[i][0] * i for i in range(gray_level_maximum_range)])
-    return math.ceil(total_sum / total_pixels)
+    
+    if DEBUG:
+        # Ploteamos el histograma con el punto de separación
+        plt.plot(hist)
+        plt.axvline(x=total_sum / total_pixels, color='r', linestyle='--')
+        plt.title('Histograma')
+        plt.show()
+
+    return round(total_sum / total_pixels)
 
 def get_min_gray_level(hist):
     for i in range(hist.shape[0]):
@@ -25,9 +34,9 @@ def get_max_gray_level(hist):
 
     return -1
 
-def quantify_hist(hist, pl_1, pl_2, pl_3):
+def quantify_hist(hist, pl_1, pl_2, pl_3, start=0, end=256):
     quantified_hist = np.zeros_like(hist)
-    for i in range(len(hist)):
+    for i in range(start, min(end, len(hist))):
         count = hist[i][0]
         if count <= pl_1:
             quantified_hist[i][0] = pl_1
@@ -41,7 +50,7 @@ def quantify_hist(hist, pl_1, pl_2, pl_3):
 
 
 
-image_gray = cv2.imread('image_from_paper.png', cv2.IMREAD_GRAYSCALE)
+image_gray = cv2.imread('40.png', cv2.IMREAD_GRAYSCALE)
 
 # Verificar si la imagen se ha cargado correctamente
 if image_gray is None:
@@ -49,13 +58,14 @@ if image_gray is None:
     exit(1)
 
 # Obtengo el histograma de la imagen
-hist = cv2.calcHist([image_gray], [0], None, [256], [0, 256])
+hist = cv2.calcHist([image_gray], [0], None, [256], [0, 256]).astype(int)
 
 # Obtenemos el nivel de gris mínimo y máximo del histograma
 min_gray_level = get_min_gray_level(hist)
 max_gray_level = get_max_gray_level(hist)
-print("MIN GRAY LEVEL", min_gray_level)
-print("MAX GRAY LEVEL", max_gray_level)
+if DEBUG:
+    print("MIN GRAY LEVEL", min_gray_level)
+    print("MAX GRAY LEVEL", max_gray_level)
 
 # Obtenemos la cantidad total de pixeles de la imagen
 total_pixels = image_gray.shape[0] * image_gray.shape[1]
@@ -67,69 +77,76 @@ sp = get_separate_point(hist, total_pixels)
 low_sub_hist = hist[:sp + 1]
 low_sub_hist_total_pixels = np.sum(low_sub_hist)
 low_sub_hist_sp = get_separate_point(low_sub_hist, low_sub_hist_total_pixels)
+if DEBUG:
+    print("LOW SUB HIST SP", low_sub_hist_sp)
 
 high_sub_hist = hist[sp + 1:]
 high_sub_hist_total_pixels = np.sum(high_sub_hist)
 # TODO: Definir si hay que sumar sp o sp + 1 a 
 high_sub_shift = sp + 1
 high_sub_hist_sp = get_separate_point(high_sub_hist, high_sub_hist_total_pixels) + high_sub_shift
+if DEBUG:
+    print("HIGH SUB HIST SP", high_sub_hist_sp)
 
 
 # Calculamos los ratios de niveles de gris (gr) del subhistograma inferior y superior
 gr_low_2 = (sp - low_sub_hist_sp) / (sp - min_gray_level)
 gr_high_2 = (max_gray_level - high_sub_hist_sp) / (max_gray_level - sp)
-print("GRL2:", gr_low_2)
-print("GRH2:",gr_high_2)
+if DEBUG:
+    print("GRL2:", gr_low_2)
+    print("GRH2:",gr_high_2)
 
 # Calculamos la diferencia de niveles de gris (d) para el subhistograma inferior y superior
 d_low = (1 - gr_low_2) / 2 if gr_low_2 > 0.5 else gr_low_2 / 2
 d_high = (1 - gr_high_2) / 2 if gr_high_2 > 0.5 else gr_high_2 / 2
-
-print("DL", d_low)
-print("DH", d_high)
+if DEBUG:
+    print("DL", d_low)
+    print("DH", d_high)
 
 # Calculamos todos los ratios de niveles de gris del subhistograma inferior
 gr_low_1 = gr_low_2 - d_low
 gr_low_3 = gr_low_2 + d_low
-print("GRL1", gr_low_1)
-print("GRL3", gr_low_3)
+if DEBUG:
+    print("GRL1", gr_low_1)
+    print("GRL3", gr_low_3)
 
 # Calculamos todos los ratios de niveles de gris del subhistograma superior
 gr_high_1 = gr_high_2 - d_high
 gr_high_3 = gr_high_2 + d_high
-print("GRH1", gr_high_1)
-print("GRH3", gr_high_3)
+if DEBUG:
+    print("GRH1", gr_high_1)
+    print("GRH3", gr_high_3)
 
 # Obtenemos los picos de cantidad de pixeles de cada subhistograma
 pk_low = low_sub_hist.max()
 pk_high = high_sub_hist.max()
-
-print("PKLOW", low_sub_hist.max())
-print("PKHigh", high_sub_hist.max())
+if DEBUG:
+    print("PKLOW", low_sub_hist.max())
+    print("PKHigh", high_sub_hist.max())
 
 # Calculamos los límites de plateau (pl) de cada subhistrograma
 pl_low_1 = gr_low_1 * pk_low
 pl_low_2 = gr_low_2 * pk_low
 pl_low_3 = gr_low_3 * pk_low
-
-print("PL1", pl_low_1)
-print("PL2", pl_low_2)
-print("PL3", pl_low_3)
+if DEBUG:
+    print("PL1", pl_low_1)
+    print("PL2", pl_low_2)
+    print("PL3", pl_low_3)
 
 pl_high_1 = gr_high_1 * pk_high
 pl_high_2 = gr_high_2 * pk_high
 pl_high_3 = gr_high_3 * pk_high
-
-print("PH1", pl_high_1)
-print("PH2", pl_high_2)
-print("PH3", pl_high_3)
+if DEBUG:
+    print("PH1", pl_high_1)
+    print("PH2", pl_high_2)
+    print("PH3", pl_high_3)
 
 # Cuantificamos los subhistogramas a partir de los limites de plateau obtenidos
-low_sub_hist_quantified = quantify_hist(low_sub_hist, pl_low_1, pl_low_2, pl_low_3)
-high_sub_hist_quantified = quantify_hist(high_sub_hist, pl_high_1, pl_high_2, pl_high_3)
+low_sub_hist_quantified = quantify_hist(low_sub_hist, pl_low_1, pl_low_2, pl_low_3, start=min_gray_level)
+high_sub_hist_quantified = quantify_hist(high_sub_hist, pl_high_1, pl_high_2, pl_high_3, end=max_gray_level - sp)
 
-low_sub_hist_quantified_total_pixels = low_sub_hist_quantified.sum()
-high_sub_hist_quantified_total_pixels = high_sub_hist_quantified.sum()
+low_sub_hist_quantified_total_pixels = low_sub_hist_quantified[min_gray_level:].sum()
+high_sub_hist_quantified_total_pixels = high_sub_hist_quantified[:max_gray_level+1].sum()
 
 # Mosramos el histograma original, los límites de plateau y el histograma cuantificado de cada subhistograma
 plt.plot(hist)
@@ -166,9 +183,8 @@ plt.show()
 
 # Procedemos a calcular la imagen ecualizada
 
-# Acá no tengo claro si esta función está bien, ya que en el paper en ningún momento se utiliza la variable k
 cdf_low = lambda k: sum([ low_sub_hist_quantified[i][0] / low_sub_hist_quantified_total_pixels  for i in range(min_gray_level, k + 1)])
-cdf_high = lambda k : sum([ high_sub_hist_quantified[i][0] / high_sub_hist_quantified_total_pixels  for i in range(k - high_sub_shift , max_gray_level + 1 - high_sub_shift)])
+cdf_high = lambda k : sum([ high_sub_hist_quantified[i][0] / high_sub_hist_quantified_total_pixels  for i in range(0, k - sp)])
 
 
 # Calculamos la transformación de los niveles de grises aplicar a la imagen
@@ -186,6 +202,11 @@ for i in range(transformed_image.shape[0]):
         new_gray_level = trans_map.get(image_gray[i][j])
         transformed_image[i][j] = new_gray_level
 
+# Mostramos el histograma de la imagen ecualizada
+histogram_transformed = cv2.calcHist([transformed_image], [0], None, [256], [0, 256]).astype(int)
+plt.bar([i for i in range(256)], histogram_transformed.ravel())
+plt.title('Histograma Ecualizado')
+plt.show()
 
 cv2.imshow("Imagen original", image_gray)
 cv2.imshow('Imagen ecualizada', transformed_image)
